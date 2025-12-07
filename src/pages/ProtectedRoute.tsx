6e32ba1c5ebb05f471efadfcf3a5f9135e7f5048
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-12-07 17:18:03 +0200

    Delete src directory

--- src/pages/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, } from 'react-router-dom';
-
-const ProtectedRoute = ({ element: Component, ...rest }) => {
-    const token = localStorage.getItem("token"); // Check if token cookie exists
-
-    return token ? <Component {...rest} /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
